430903b3cacfbd32d71deabb5fa0f3e47f8e77e1
Hristomira Sultova <hsultova@gmail.com>
2017-05-24 14:28:15 +0300

    #2 Books Added Genre edit and list actions

--- BookReader/BookReader.Web/Controllers/GenreController.cs
+++ BookReader/BookReader.Web/Controllers/GenreController.cs
@@ -17,6 +17,14 @@ namespace BookReader.Web.Controllers
 			_genreRepository = genreRepository;
 		}
 
+		[Authorize]
+		[HttpGet]
+		public IActionResult Index()
+		{
+			IList<Genre> genres = _genreRepository.LoadList();
+			return View(genres);
+		}
+
 		[Authorize(Policy = BookReaderPolicies.AdminPolicy)]
 		[HttpGet]
 		public IActionResult Create()
@@ -41,11 +49,61 @@ namespace BookReader.Web.Controllers
 					var genre = new Genre { Name = model.Name };
 
 					_genreRepository.Add(genre);
-					return RedirectToAction("Index", "Home");
+					return RedirectToAction("Index", "Genre");
 				}
 			}
 
 			return View(model);
+		}
+
+		[Authorize(Policy = BookReaderPolicies.AdminPolicy)]
+		[HttpGet]
+		public IActionResult Edit(int id)
+		{
+			Genre genre = _genreRepository.Load(id);
+			var model = new GenreViewModel
+			{
+				Id = genre.Id,
+				Name = genre.Name
+			};
+
+			return View(model);
+		}
+
+		[Authorize(Policy = BookReaderPolicies.AdminPolicy)]
+		[HttpPost]
+		public IActionResult Edit(GenreViewModel model)
+		{
+			if (ModelState.IsValid)
+			{
+				List<string> genreNames = _genreRepository.GetGenreNames();
+
+				if (genreNames.Contains(model.Name))
+				{
+					ModelState.AddModelError("DuplicateGenreName", "The Genre name already exist.");
+				}
+				else
+				{
+					Genre genre = _genreRepository.Load(model.Id);
+					genre.Name = model.Name;
+
+					_genreRepository.Save(genre);
+					return RedirectToAction("Index", "Genre");
+				}
+			}
+
+			return View(model);
+		}
+
+		[Authorize(Policy = BookReaderPolicies.AdminPolicy)]
+		[HttpPost]
+		public IActionResult Delete(int id)
+		{
+			Genre genre = _genreRepository.Load(id);
+
+			_genreRepository.Remove(genre);
+
+			return RedirectToAction("Index", "Genre");
 		}
 	}
 }
--- BookReader/BookReader.Web/ViewModels/Genre/GenreViewModel.cs
+++ BookReader/BookReader.Web/ViewModels/Genre/GenreViewModel.cs
@@ -4,6 +4,8 @@ namespace BookReader.Web.ViewModels.Genre
 {
 	public class GenreViewModel
 	{
+		public int Id { get; set; }
+
 		[Required]
 		public string Name { get; set; }
 	}
